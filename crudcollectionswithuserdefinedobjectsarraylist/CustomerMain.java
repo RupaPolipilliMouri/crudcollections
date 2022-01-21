ba505eb6ead0c0c39a9a284b161ea26d0c2d5ddf
RupaPolipilliMouri <96765255+RupaPolipilliMouri@users.noreply.github.com>
2022-01-21 09:10:02 +0530

    Add files via upload

---
+++ crudcollectionswithuserdefinedobjectsarraylist/CustomerMain.java
@@ -0,0 +1,12 @@
+package crudcollectionswithuserdefinedobjectsarraylist;
+
+public class CustomerMain {
+
+	public static void main(String[] args) {
+
+		CustomerOperations cop = new CustomerOperations();
+		
+		cop.showProductOperations();
+	}
+
+}
